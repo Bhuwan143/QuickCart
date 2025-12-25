a8d5b56adfaaf1d40f063fa8c08a0f1f2018d6ce
Bhuwan Bhatt <167435897+Bhuwan143@users.noreply.github.com>
2025-12-25 22:26:02 +0530

    clean vercel warnings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
